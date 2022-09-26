ec8a1ae7dd100ebee64c6dd4351bfebea7ad48bc
mjsand <63204640+mjsand@users.noreply.github.com>
2022-09-25 17:53:40 -0700

    Add files via upload

---
+++ roman.py
@@ -0,0 +1,17 @@
+def romanToInt(s):
+    # create a map of each roman numeral and its corresponding value    
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # Length of the given string
+        n = len(s)
+    # This variable will store result
+        num = roman_map[s[n - 1]]
+    # Loop for each character from right to left
+        for i in range(n - 2, -1, -1):
+        # Check if the character at right of current character is bigger or smaller
+        #if character to the left is larger, add its value to our total
+            if roman_map[s[i]] >= roman_map[s[i + 1]]:
+                num += roman_map[s[i]]
+                #if its smaller, subtract from our total
+            else:
+                num -= roman_map[s[i]]
+        return num
